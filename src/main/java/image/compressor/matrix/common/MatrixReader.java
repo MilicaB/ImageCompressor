08fd09e2b46f87e016510f93a3a55f25d94341bc
Militsa <bucibuci67@abv.bg>
2016-03-23 00:42:19 +0200

    Initial files
    Adding class for matrix representation + general operations and class
    for reading a matrix from pgm file

---
+++ src/main/java/image/compressor/matrix/common/MatrixReader.java
@@ -0,0 +1,50 @@
+package image.compressor.matrix.common;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MatrixReader {
+	public static Matrix readMatrixFromPgmFile(String fileName) {
+		Matrix result;
+		FileInputStream fileInputStream = null;
+		Scanner scan = null;
+		try {
+			fileInputStream = new FileInputStream(fileName);
+			scan = new Scanner(fileInputStream);
+			// Discard the magic number
+			scan.nextLine();
+			// Discard the comment line
+			scan.nextLine();
+			// Read pic width, height and max value
+			int picWidth = scan.nextInt();
+			int picHeight = scan.nextInt();
+			// Discard the max value
+			// TODO militsa - is this max value needed??!!
+			scan.nextInt();
+			result = new Matrix(picHeight, picWidth);
+			for (int i = 0; i < picHeight; i++) {
+				for (int j = 0; j < picWidth; j++) {
+					result.addElementToMatrix(i, j, scan.nextInt());
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			result = new Matrix();
+		} finally {
+			try {
+				fileInputStream.close();
+				scan.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		//sample usage
+		MatrixReader.readMatrixFromPgmFile("C:\\file.pgm").print();
+	}
+
+}
