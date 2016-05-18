3a6075cddb14b5b2050e183b77769145c2dd0758
Gabi <gg_grozeva@mail.bg>
2016-05-18 20:26:38 +0300

    All perfecr

--- src/main/java/image/compressor/matrix/common/SVD.java
+++ src/main/java/image/compressor/matrix/common/SVD.java
@@ -12,15 +12,14 @@ import java.lang.*;
 import java.util.LinkedList;
 import java.util.List;
 
-import pitt.search.semanticvectors.vectors.Vector;
+//import pitt.search.semanticvectors.vectors.Vector;
 
 /**
  *
  * @author Gabi
  */
 public class SVD {
-<<<<<<< HEAD
-    
+
      public static double normCol(double [] a ){
             double sum = 0;
             for( int i = 0; i < a.length; i++){
@@ -120,98 +119,4 @@ public class SVD {
             return result;
         }
     
-=======
-
-	public static double normCol(double[] a) {
-		double sum = 0;
-		for (int i = 0; i < a.length; i++) {
-			double num = a[i];
-			sum += num * num;
-		}
-		return Math.sqrt(sum);
-	}
-
-	/**
-	 * From eigen values to singular
-	 * 
-	 * @param eigen
-	 *            array with eigen values
-	 * @param savedEigenValues
-	 *            the number of searched singular values
-	 * @return array with singular values
-	 */
-	public static double[] singularValues(double[] eigen, int savedEigenValues) {
-		double[] singular = new double[savedEigenValues];
-		for (int i = 0; i < savedEigenValues; i++) {
-			singular[i] = Math.sqrt(eigen[eigen.length - 1 - i]);
-		}
-
-		return singular;
-	}
-
-	/**
-	 * Compression of Matrix with SVD algorithm
-	 * 
-	 * @param rawPicMatrix
-	 * @param percent
-	 * @return
-	 */
-	public static Matrix SVD(Matrix rawPicMatrix, int percent) {
-
-		Matrix A = rawPicMatrix;
-		int rowCount = rawPicMatrix.getRowDimension();
-		int colCount = rawPicMatrix.getColumnDimension();
-		Matrix symmetricRawPicMatrix = rawPicMatrix.transpose().times(A);
-
-		EigenvalueDecomposition a = symmetricRawPicMatrix.eig();
-		double[] eigenValues = a.getRealEigenvalues();
-
-		int nonzeroVal = 0;
-		for (int i = 0; i < colCount; i++) {
-			if (eigenValues[i] > 0) {
-				nonzeroVal++;
-			}
-		}
-
-		int saved = (int) (nonzeroVal);
-		double[] singular = singularValues(eigenValues, saved);
-
-		Matrix eigenVectors = switchCols(a.getV());
-//		Matrix eigenVectors=a.getV();
-
-		Matrix U = new Matrix(rowCount, saved);
-		Matrix temp = A.times(eigenVectors);
-
-		for (int i = 0; i < rowCount; i++) {
-			for (int j = 0; j < saved; j++) {
-				U.set(i, j, temp.get(i, j) / singular[j]);
-			}
-		}
-
-		Matrix singularMatrix = new Matrix(saved, colCount);
-		int compressed = (int) (saved * (1.0 - percent / 100.0));
-		for (int i = 0; i < compressed; i++) {
-			System.out.println(singular[i]);
-			singularMatrix.set(i, i, singular[i]);
-		}
-
-		Matrix times = U.times(singularMatrix);
-		Matrix result = times.times(eigenVectors);
-		return result;
-	}
-
-	private static Matrix switchCols(Matrix v) {
-		int rows = v.getRowDimension();
-		int cols = v.getColumnDimension();
-		double[][] result = new double[rows][cols];
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				result[i][j] = v.get(i, cols - j - 1);
-			}
-		}
-		List<Vector> vectors = new LinkedList<Vector>();
-		
-		return new Matrix(result);
-	}
->>>>>>> e01542f51b184f5a0dc2d108157ccf820888fc3f
 }
--- src/main/java/image/compressor/matrix/common/image/converter/ImageUtil.java
+++ src/main/java/image/compressor/matrix/common/image/converter/ImageUtil.java
@@ -1,3 +1,113 @@
+//package image.compressor.matrix.common.image.converter;
+//
+//import java.awt.Color;
+//import java.awt.image.BufferedImage;
+//import java.io.File;
+//import java.io.IOException;
+//
+//import javax.imageio.ImageIO;
+//
+//import Jama.Matrix;
+//import image.compressor.matrix.common.SVD;
+//
+//public class ImageUtil {
+//
+//	/**
+//	 * Convert RGB image to greyscale
+//	 * 
+//	 * @param imagePath
+//	 * @param greyScaleImagePath
+//	 */
+//	public static void turnToGreyScale(String imagePath,
+//			String greyScaleImagePath) {
+//		BufferedImage image;
+//		int width;
+//		int height;
+//		try {
+//			File input = new File(imagePath);
+//			image = ImageIO.read(input);
+//			width = image.getWidth();
+//			height = image.getHeight();
+//
+//			for (int i = 0; i < height; i++) {
+//				for (int j = 0; j < width; j++) {
+//					Color c = new Color(image.getRGB(j, i));
+//					int red = (int) (c.getRed() * 0.299);
+//					int green = (int) (c.getGreen() * 0.587);
+//					int blue = (int) (c.getBlue() * 0.114);
+//					Color newColor = new Color(red + green + blue, red + green
+//							+ blue, red + green + blue);
+//					image.setRGB(j, i, newColor.getRGB());
+//				}
+//			}
+//			File ouptut = new File(greyScaleImagePath);
+//			ImageIO.write(image, ImageFormat.JPEG.getFormat(), ouptut);
+//		} catch (Exception e) {
+//		}
+//	}
+//
+//	/**
+//	 * Converts image to matrix
+//	 * 
+//	 * @param imagePath
+//	 * @return
+//	 */
+//	public static Matrix getImageAsMatrix(String imagePath) {
+//		File img = new File(imagePath);
+//		BufferedImage image = null;
+//		try {
+//			image = ImageIO.read(img);
+//		} catch (IOException e) {
+//			System.err.println("Unable to find file");
+//			e.printStackTrace();
+//		}
+//		int width = image.getWidth();
+//		int height = image.getHeight();
+//		double[][] result = new double[height][width];
+//
+//		for (int row = 0; row < height; row++) {
+//			for (int col = 0; col < width; col++) {
+//				result[row][col] = image.getRGB(col, row);
+//			}
+//		}
+//		return new Matrix(result);
+//	}
+//
+//	/**
+//	 * Converts matrix to image
+//	 * 
+//	 * @param matrix
+//	 * @param imagePath
+//	 */
+//	public static void createImageFromMatrix(Matrix matrix, String imagePath) {
+//		int width = matrix.getColumnDimension();
+//		int height = matrix.getRowDimension();
+//		BufferedImage image = new BufferedImage(width, height,
+//				BufferedImage.TYPE_INT_RGB);
+//
+//		for (int row = 0; row < height; row++) {
+//			for (int col = 0; col < width; col++) {
+//				image.setRGB(col, row, (int) matrix.get(row, col));
+//			}
+//		}
+//		File outputfile = new File(imagePath);
+//		try {
+//			ImageIO.write(image, ImageFormat.JPEG.getFormat(), outputfile);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//	}
+//
+//	static public void main(String args[]) throws Exception {
+//		Matrix imageAsMatrix = ImageUtil
+//				.getImageAsMatrix("E:\\Pictures\\download.jpg");
+//                
+//		ImageUtil.createImageFromMatrix(SVD.SVD(imageAsMatrix, 90),
+//				"E:\\Pictures\\download1.jpg");
+//	}
+//}
 package image.compressor.matrix.common.image.converter;
 
 import image.compressor.matrix.common.SVD;
@@ -12,152 +122,138 @@ import Jama.Matrix;
 import image.compressor.matrix.common.SVD;
 
 public class ImageUtil {
-	private Matrix redMatrix;
-	private Matrix blueMatrix;
-	private Matrix greenMatrix;
-	private String imagePath;
 
-	public ImageUtil(String imagePath) {
-		this.imagePath = imagePath;
-		createMatrices();
-	}
+    private Matrix redMatrix;
+    private Matrix blueMatrix;
+    private Matrix greenMatrix;
+    private String imagePath;
 
-	/**
-	 * Converts image to matrix
-	 * 
-	 * @param imagePath
-	 */
-	private void createMatrices() {
-		File img = new File(imagePath);
-		BufferedImage image = null;
-		try {
-			image = ImageIO.read(img);
-		} catch (IOException e) {
-			System.err.println("Unable to find file");
-			e.printStackTrace();
-		}
-		int width = image.getWidth();
-		int height = image.getHeight();
-		double[][] red = new double[height][width];
-		double[][] green = new double[height][width];
-		double[][] blue = new double[height][width];
+    public ImageUtil(String imagePath) {
+        this.imagePath = imagePath;
+        createMatrices();
+    }
 
-		for (int row = 0; row < height; row++) {
-			for (int col = 0; col < width; col++) {
-				int rgb = image.getRGB(row, col);
-				Color color = new Color(rgb);
-				blue[row][col] = color.getBlue();
-				red[row][col] = color.getRed();
-				green[row][col] = color.getGreen();
-			}
-		}
-		redMatrix = new Matrix(red);
-		greenMatrix = new Matrix(green);
-		blueMatrix = new Matrix(blue);
-	}
+    /**
+     * Converts image to matrix
+     *
+     * @param imagePath
+     */
+    private void createMatrices() {
+        File img = new File(imagePath);
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(img);
+        } catch (IOException e) {
+            System.err.println("Unable to find file");
+            e.printStackTrace();
+        }
+        int width = image.getWidth();
+        int height = image.getHeight();
+        double[][] red = new double[height][width];
+        double[][] green = new double[height][width];
+        double[][] blue = new double[height][width];
 
-	private void createImageFromThreeMatrices(Matrix red, Matrix green,
-			Matrix blue, String imagePathNew) {
-		int width = red.getColumnDimension();
-		int height = red.getRowDimension();
-		BufferedImage image = new BufferedImage(width, height,
-				BufferedImage.TYPE_INT_RGB);
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                try{
+                int rgb = image.getRGB(col,row);
+                Color color = new Color(rgb);
+                blue[row][col] = color.getBlue();
+                red[row][col] = color.getRed();
+                green[row][col] = color.getGreen();}
+                catch(ArrayIndexOutOfBoundsException e){
+                    System.out.println("ERROR");
+                }
+            }
+        }
+        redMatrix = new Matrix(red);
+        greenMatrix = new Matrix(green);
+        blueMatrix = new Matrix(blue);
+    }
 
-		for (int row = 0; row < height; row++) {
-			for (int col = 0; col < width; col++) {
-				int r = (int) red.get(row, col);
-				if (r < 0) {
-					r = 0;
-				}
-				if (r > 255) {
-					r = 255;
-				}
-				int g = (int) green.get(row, col);
-				if (g < 0) {
-					g = 0;
-				}
-				if (g > 255) {
-					g = 255;
-				}
-				int b = (int) blue.get(row, col);
-				if (b < 0) {
-					b = 0;
-				}
-				if (b > 255) {
-					b = 255;
-				}
-				Color color = new Color(r, g, b);
-				image.setRGB(row, col, color.getRGB());
-			}
-		}
-		File outputfile = new File(imagePathNew);
-		try {
-			ImageIO.write(image, ImageFormat.JPEG.getFormat(), outputfile);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    private void createImageFromThreeMatrices(Matrix red, Matrix green,
+            Matrix blue, String imagePathNew) {
+        int width = red.getColumnDimension();
+        int height = red.getRowDimension();
+        BufferedImage image = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
 
-	public void convertImage(String newImagePath, int compression) {
-		Matrix blueMatrixCompressed = SVD.SVD(blueMatrix, compression);
-		Matrix redMatrixCompressed = SVD.SVD(redMatrix, compression);
-		Matrix greenMatrixCompressed = SVD.SVD(greenMatrix, compression);
-		createImageFromThreeMatrices(redMatrixCompressed,
-				greenMatrixCompressed, blueMatrixCompressed, newImagePath);
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                int r = (int) red.get(row, col);
+                if (r < 0) {
+                    r = 0;
+                }
+                if (r > 255) {
+                    r = 255;
+                }
+                int g = (int) green.get(row, col);
+                if (g < 0) {
+                    g = 0;
+                }
+                if (g > 255) {
+                    g = 255;
+                }
+                int b = (int) blue.get(row, col);
+                if (b < 0) {
+                    b = 0;
+                }
+                if (b > 255) {
+                    b = 255;
+                }
+                Color color = new Color(r, g, b);
+                image.setRGB( col,row, color.getRGB());
+            }
+        }
+        File outputfile = new File(imagePathNew);
+        try {
+            ImageIO.write(image, ImageFormat.JPEG.getFormat(), outputfile);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void convertImage(String newImagePath, int compression) {
+        Matrix blueMatrixCompressed = SVD.SVD(blueMatrix, compression);
+        Matrix redMatrixCompressed = SVD.SVD(redMatrix, compression);
+        Matrix greenMatrixCompressed = SVD.SVD(greenMatrix, compression);
+        createImageFromThreeMatrices(redMatrixCompressed,
+                greenMatrixCompressed, blueMatrixCompressed, newImagePath);
 //		createImageFromThreeMatrices(redMatrix,
 //				greenMatrix, blueMatrix, newImagePath);
-	}
+    }
 
-	/**
-	 * Converts matrix to image
-	 * 
-	 * @param matrix
-	 * @param imagePath
-	 */
-	public static void createImageFromMatrix(Matrix matrix, String imagePath) {
-		int width = matrix.getColumnDimension();
-		int height = matrix.getRowDimension();
-		BufferedImage image = new BufferedImage(width, height,
-				BufferedImage.TYPE_INT_RGB);
+    /**
+     * Converts matrix to image
+     *
+     * @param matrix
+     * @param imagePath
+     */
+    public static void createImageFromMatrix(Matrix matrix, String imagePath) {
+        int width = matrix.getColumnDimension();
+        int height = matrix.getRowDimension();
+        BufferedImage image = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_RGB);
 
-		for (int row = 0; row < height; row++) {
-			for (int col = 0; col < width; col++) {
-				image.setRGB(row, col, (int) matrix.get(row, col));
-			}
-		}
-		File outputfile = new File(imagePath);
-		try {
-			ImageIO.write(image, ImageFormat.JPEG.getFormat(), outputfile);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                image.setRGB(row, col, (int) matrix.get(row, col));
+            }
+        }
+        File outputfile = new File(imagePath);
+        try {
+            ImageIO.write(image, ImageFormat.JPEG.getFormat(), outputfile);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	static public void main(String args[]) throws Exception {
-<<<<<<< HEAD
-		Matrix imageAsMatrix = ImageUtil
-				.getImageAsMatrix("E:\\Pictures\\download.jpg");
-//
-                Matrix a = SVD.SVD(imageAsMatrix,20);
-//                double m [][]= {{2,0}, {1, 2}, {0, 1}};
-//                Matrix b = new Matrix(m);
-//                SVD.SVD(b, 20).print(5, 2);
-//                Matrix randomMatrix = Matrix.random(100, 100);
-//                SVD.SVD(randomMatrix, 0);
-                ImageUtil.createImageFromMatrix( a ,
-				"E:\\Pictures\\download1.jpg");
-=======
-		double[][] array= {{1,2},{2,2},{2,1}};
-		Matrix rawPicMatrix=new Matrix(array);
-		Matrix svd = SVD.SVD(rawPicMatrix, 0);
-		System.out.println(svd);
-//		ImageUtil imageConverterImageUtil = new ImageUtil(
-//				"C:\\Users\\az\\Desktop\\baboon.bmp");
-//		imageConverterImageUtil.convertImage(
-//				"C:\\Users\\az\\Desktop\\baboonCompr.bmp", 0);
->>>>>>> e01542f51b184f5a0dc2d108157ccf820888fc3f
-	}
+    static public void main(String args[]) throws Exception {
+        ImageUtil util = new ImageUtil("E:\\\\Pictures\\\\12190927_891433274226774_1005484681681057446_n.jpg");
+        util.convertImage("E:\\\\Pictures\\\\12190927_891433274226774_1005484681681057446_n1.jpg",95);
+
+    }
 }
