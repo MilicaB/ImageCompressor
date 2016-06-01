eff0563cecefffdcf88bff4f5e301833c55ee1cc
MilicaB <milicabb13@gmail.com>
2016-06-01 19:26:10 +0300

    Stubbed methods to GramSchidt class

--- src/main/java/image/compressor/matrix/common/GramSchmidt.java
+++ src/main/java/image/compressor/matrix/common/GramSchmidt.java
@@ -1,4 +1,13 @@
 package image.compressor.matrix.common;
 
 public class GramSchmidt {
+	public double getProj(int eNum, int aNum){
+		double proj=0;
+		return proj;
+	}
+	
+	public double[] getUnitVector(int unitVectorNum){
+		double[] vector;
+		return vector;
+	}
 }
